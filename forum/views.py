d8fa4195a5c876b55b28da7dbd15f52d21a3db47
Aularius <aularius@gmail.com>
2014-11-14 22:16:40 -0800

    Do login view

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as do_login
 
 # Create your views here.
 def index(request):
@@ -10,6 +11,19 @@ def forum(request,forum_name): pass
 def thread(request,id): pass
 
 def login(request):
-	return render(request, 'login.html')
+    if request.method == 'GET':
+        if request.user.is_authenticated():
+            return redirect('/')
+    	return render(request, 'login.html')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(email = email, password = password)
+        if user:
+            do_login(request, user)
+            return redirect('/')
+        else:
+            context = {'invalid_login': True}
+            return render(request, 'loginpage.html', context)
 
 def post(request):pass
