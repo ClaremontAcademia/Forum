e8753371af1e7c0c1359ae84a583c601a770819e
Ziqi Xiong <zx012013@mymail.pomona.edu>
2014-11-14 22:00:18 -0800

    flesh out register view

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from forum.models import User
+from random import randint
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
@@ -15,6 +18,14 @@ def login(request):
 def post(request):pass
 
 def register(request):
-	email = request.POST.email
+	email = request.POST['email']
 	user = User(email = email,display_name = email)
 	user.save();
+	raw_password = randint(1000000,9999999)
+    user.set_password(raw_password)
+    send_mail('Welcome to Claremont Academia!','You temporary password is '+ raw_password+'.', \
+    	'claremont_academia@yahoo.com',[email],fail_silently=False)
+    return redirect('/forums/login')
+
+
+
