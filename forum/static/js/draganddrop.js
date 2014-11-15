446f2f004469bfef3f4beb18da7bc8446ffa56fa
theworldbright <sushionigiris@gmail.com>
2014-11-14 21:31:04 -0800

    Add basic drag and drop support

--- forum/static/js/draganddrop.js
+++ forum/static/js/draganddrop.js
@@ -1,22 +1,17 @@
 $(function() {
-    $(".dropdown-menu li").draggable({
+    $(".dropdown-menu li a").draggable({
         appendTo: "body",
-        helper: "clone"
+        helper: "clone",
+        zIndex: 10000
     });
-    $(".dropdown-menu li").droppable({
+    $(".favorites").droppable({
         activeClass: "ui-state-default",
         hoverClass: "ui-state-hover",
         accept: ":not(.ui-sortable-helper)",
         drop: function(event, ui) {
-            $(this).find(".placeholder").remove();
-            $("<li></li>").text(ui.draggable.text()).appendTo(this);
-        }
-    }).sortable({
-        items: "li:not(.placeholder)",
-        sort: function() {
-            // gets added unintentionally by droppable interacting with sortable
-            // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-            $(this).removeClass("ui-state-default");
+            $(this).attr("href", ui.draggable.attr("href"));
+            $(this).find(".favorites").remove();
+            $(this).text(ui.draggable.text()).appendTo(this);
         }
     });
 });
