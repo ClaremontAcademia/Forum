1609791b0e6a677a544ffa20728cc30a9f9c8e98
Ziqi Xiong <zx012013@mymail.pomona.edu>
2014-11-14 22:28:14 -0800

    validate email'

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from forum.models import User
 from random import randint
 from django.core.mail import send_mail
+import re
 
 # Create your views here.
 def index(request):
@@ -19,14 +20,20 @@ def post(request):pass
 
 def register(request):
 	email = request.POST['email']
-
-	user = User(email = email,display_name = email)
-	user.save();
-	raw_password = randint(1000000,9999999)
-    user.set_password(raw_password)
-    send_mail('Welcome to Claremont Academia!','You temporary password is '+ raw_password+'.', \
+    if validateEmail(email):
+	  user = User(email = email,display_name = email)
+	  user.save();
+	  raw_password = randint(1000000,9999999)
+      user.set_password(raw_password)
+      send_mail('Welcome to Claremont Academia!','You temporary password is '+ raw_password+'.', \
     	'claremont_academia@yahoo.com',[email],fail_silently=False)
-    return redirect('login/')
+      return redirect('login/')
+    else: render(request,'login.html',{'invalidate_email':True})
+
+def validateEmail (email):
+	if re.match('\w+@pomona.edu$',email) ! = None:
+		return True
+	else return False
 
 
 
