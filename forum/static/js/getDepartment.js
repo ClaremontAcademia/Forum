edae2d7ac0cbc8b4b7fc28bb02dcd2d37db3bbd0
theworldbright <sushionigiris@gmail.com>
2014-11-15 04:04:48 -0800

    Move javascript for post.html to getDepartment.js

---
+++ forum/static/js/getDepartment.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    alert("I am an alert box!");
+});
+
+function myFunction() {
+    var department_name = $(this).val();
+    var request_url = '/get_department/' + department_name + "/";
+    $.ajax({
+        url: request_url,
+        success: function(data) {
+            $.each(data[0], function(key, value) {
+                $('select[name=course]').append('<option value="' + this.key + '">' + this.value + '</option>');
+            });
+            return false;
+        }
+    })
+}
