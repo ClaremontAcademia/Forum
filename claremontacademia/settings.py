08b90bea2ba4015e7a67bb3e8a65af17c44a1ecd
theworldbright <sushionigiris@gmail.com>
2015-01-12 02:04:48 +0900

    Change static_url directory to match Nginx's specification

--- claremontacademia/settings.py
+++ claremontacademia/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/opt/myenv/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.mail.yahoo.com"
