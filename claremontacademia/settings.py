819f718663ff706e658c1ad81aa8b86c0cc0d36f
theworldbright <sushionigiris@gmail.com>
2015-06-16 11:06:43 +0900

    Update static root

--- claremontacademia/settings.py
+++ claremontacademia/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = "/opt/myenv/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
